73c08aaa2d3cce5b6d0152518d57c3075a22ceff
NoeSaltos <noeasalt@espol.edu.ec>
2023-10-29 13:27:37 -0500

    otro commit

--- agenda/src/main/resources/com/mycompany/agenda/GestorContactosController.java
+++ agenda/src/main/resources/com/mycompany/agenda/GestorContactosController.java
@@ -9,6 +9,8 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -19,6 +21,36 @@ public class GestorContactosController implements Initializable {
 
     @FXML
     private Button bteditar;
+    @FXML
+    private Button btagregar;
+    @FXML
+    private Tab tabpersona;
+    @FXML
+    private Tab tabempresa;
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtApellido;
+    @FXML
+    private TextField txtEmpresa;
+    @FXML
+    private TextField txtEmailPer;
+    @FXML
+    private TextField txtEmailTra;
+    @FXML
+    private TextField txtTelf;
+    @FXML
+    private TextField txtTelfTra;
+    @FXML
+    private TextField txtTelfCasa;
+    @FXML
+    private TextField txtDireccion;
+    @FXML
+    private TextField txtdireccionTrabajo;
+    @FXML
+    private Button btsiguiente;
+    @FXML
+    private Button btanterior;
 
     /**
      * Initializes the controller class.
