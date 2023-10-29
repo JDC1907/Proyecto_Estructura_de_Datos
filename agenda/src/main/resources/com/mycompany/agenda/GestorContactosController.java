18697b91ced13e9bdb843a73efbb62836b60d75a
NoeSaltos <noeasalt@espol.edu.ec>
2023-10-29 13:01:31 -0500

    commit

---
+++ agenda/src/main/resources/com/mycompany/agenda/GestorContactosController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.agenda;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author nhale
+ */
+public class GestorContactosController implements Initializable {
+
+    @FXML
+    private Button bteditar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
